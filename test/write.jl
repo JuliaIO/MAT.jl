b66936319e7b5a6fc85a57916da8f72372826582
Simon Kornblith <simon@simonster.com>
2012-12-06 15:34:43 -0500

    Add write test from julia_hdf5

---
+++ test/write.jl
@@ -0,0 +1,15 @@
+load("src/MAT")
+using MAT
+
+fid = matopen("/tmp/matwrite.mat","w")
+A = randi(20, 3, 5)
+B = randn(2, 4)
+AB = Any[A, B]
+write(fid, "AB", AB)
+type MyStruct
+    len::Int
+    data::Array{Float64}
+end
+ms = MyStruct(2, [3.2, -1.7])
+write(fid, "mystruct", ms)
+close(fid)
