507b81e827634cfe9a9e084d7e9504f40524eaf8
Simon Kornblith <simon@simonster.com>
2012-12-09 23:55:23 -0500

    Add more comprehensive tests based on tests for reading. These currently fail.

--- test/write.jl
+++ test/write.jl
@@ -1,15 +1,58 @@
 load("src/MAT")
 using MAT
 
-fid = matopen("/tmp/matwrite.mat","w")
-A = randi(20, 3, 5)
-B = randn(2, 4)
-AB = Any[A, B]
-write(fid, "AB", AB)
-type MyStruct
-    len::Int
-    data::Array{Float64}
+function test_write(data)
+	fid = matopen("/tmp/matwrite.mat", "w")
+	for (k, v) in data
+		write(fid, k, v)
+	end
+	close(fid)
+	fid = matopen("/tmp/matwrite.mat", "r")
+	result = read(fid)
+	close(fid)
+	if result != data
+		error("Data mismatch")
+	end
 end
-ms = MyStruct(2, [3.2, -1.7])
-write(fid, "mystruct", ms)
-close(fid)
+
+test_write({
+	"int8" => int8(1),
+	"uint8" => uint8(1),
+	"int16" => int16(1),
+	"uint16" => uint16(1),
+	"int32" => int32(1),
+	"uint32" => uint32(1),
+	"int64" => int64(1),
+	"uint64" => uint64(1),
+	"single" => float32(1),
+	"double" => float64(1)
+})
+
+test_write({
+	"simple_string" => "the quick brown fox",
+	"accented_string" => "thé qüîck browñ fòx",
+	"concatenated_strings" => ["this is a string", "this is another string"],
+	"cell_strings" => ["this is a string" "this is another string"]
+})
+
+test_write({
+	"a1x2" => [1.0 2.0],
+	"a2x1" => zeros(2, 1)+[1.0, 2.0],
+	"a2x2" => [1.0 3.0; 4.0 2.0],
+	"a2x2x2" => cat(3, [1.0 3.0; 4.0 2.0], [1.0 2.0; 3.0 4.0]),
+	"empty" => zeros(0, 0),
+	"string" => "string"
+})
+
+test_write({
+	"cell" => {1 2.01 "string" {"string1" "string2"}}
+})
+
+test_write({
+	"s" => {
+		"a" => 1.0,
+		"b" => [1.0 2.0],
+		"c" => [1.0 2.0 3.0]
+	},
+	"s2" => { "a" => [1.0 2.0] }
+})
