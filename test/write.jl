06a255e8db96863d3d68d997c45e93fb09cad549
Simon Kornblith <simon@simonster.com>
2013-01-11 16:35:33 -0500

    Use Complex128 in test

--- test/write.jl
+++ test/write.jl
@@ -30,7 +30,7 @@ test_write({
 	"double" => float64(1)
 })
 
-test_write({ "imaginary" => [1 -1 1+im 1-im -1+im -1-im im] })
+test_write({ "imaginary" => [1.0 -1.0 1.0+1.0im 1.0-1.0im -1.0+1.0im -1.0-1.0im 1.0im] })
 
 test_write({
 	"simple_string" => "the quick brown fox",
