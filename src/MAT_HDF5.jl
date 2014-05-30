efaca553b4a195af262d6881709349ae0cceb0a9
Simon Kornblith <simon@simonster.com>
2014-05-30 19:58:22 +0200

    Fix #26

--- src/MAT_HDF5.jl
+++ src/MAT_HDF5.jl
@@ -539,9 +539,9 @@ function read(obj::HDF5Object, ::Type{MatlabString})
         data = reshape(data, sz[2:end])
     end
     if ndims(data) == 1
-        return bytestring(CharString(data))
+        return bytestring(utf16(data))
     elseif ndims(data) == 2
-        return datap = ByteString[rstrip(bytestring(CharString(reshape(data[i, :], size(data,2))))) for i = 1:size(data, 1)]
+        return datap = ByteString[rstrip(bytestring(utf16(reshape(data[i, :], size(data,2))))) for i = 1:size(data, 1)]
     else
         return data
     end
