4303ad305ed3987b195286bd10ca9ddfb33f20da
Nicole Epp <nicole.epp@invenia.ca>
2018-10-04 15:48:23 -0500

    Update for julia 1.0

--- src/MAT.jl
+++ src/MAT.jl
@@ -26,7 +26,8 @@ VERSION >= v"0.4.0-dev+6521" && __precompile__()
 
 module MAT
 
-using HDF5, Compat
+using HDF5
+using Compat: AbstractDict, undef
 
 include("MAT_HDF5.jl")
 include("MAT_v5.jl")
@@ -53,7 +54,7 @@ function matopen(filename::AbstractString, rd::Bool, wr::Bool, cr::Bool, tr::Boo
     rawfid = open(filename, "r")
 
     # Check for MAT v4 file
-    magic = read!(rawfid, Vector{UInt8}(4))
+    magic = read!(rawfid, Vector{UInt8}(undef, 4))
     for i = 1:length(magic)
         if magic[i] == 0
             close(rawfid)
@@ -76,7 +77,7 @@ function matopen(filename::AbstractString, rd::Bool, wr::Bool, cr::Bool, tr::Boo
     # Check for HDF5 file
     for offset = 512:512:fs-8
         seek(rawfid, offset)
-        if read!(rawfid, Vector{UInt8}(8)) == HDF5_HEADER
+        if read!(rawfid, Vector{UInt8}(undef, 8)) == HDF5_HEADER
             close(rawfid)
             return MAT_HDF5.matopen(filename, rd, wr, cr, tr, ff)
         end
@@ -143,7 +144,7 @@ end
 Write a dictionary containing variable names as keys and values as values
 to a Matlab file, opening and closing it automatically.
 """
-function matwrite{S, T}(filename::AbstractString, dict::Associative{S, T})
+function matwrite(filename::AbstractString, dict::AbstractDict{S, T}) where {S, T}
     file = matopen(filename, "w")
     try
         for (k, v) in dict
