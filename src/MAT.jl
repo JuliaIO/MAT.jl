2d7333cd4a98e22ce0f4e2909e95688f0b44657a
Jeff Fessler <fessler@umich.edu>
2022-11-22 21:41:43 -0500

    Tweak docstring

--- src/MAT.jl
+++ src/MAT.jl
@@ -109,10 +109,11 @@ end
     matopen(filename [, mode]; compress = false) -> handle
     matopen(f::Function, filename [, mode]; compress = false) -> f(handle)
 
-Mode defaults to "r" for read.  It can also be "w" for write, or "r+" for
-read or write without creation or truncation.
+Mode defaults to `"r"` for read.
+It can also be `"w"` for write,
+or `"r+"` for read or write without creation or truncation.
 
-Compression on reading is detected/handled automatically; the compress
+Compression on reading is detected/handled automatically; the `compress`
 keyword argument only affects write operations.
 
 Use with `read`, `write`, `close`, `keys`, and `haskey`.
